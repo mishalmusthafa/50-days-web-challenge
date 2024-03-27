c64bf9b6cf049d207678ff0c7159e72db9b8307d
Mishal <mishalmusthafa12@gmail.com>
2024-03-27 19:25:07 +0530

    initial commit

---
+++ expanding_cards/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+function removeActivePanel() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActivePanel();
+    panel.classList.add('active');
+  });
+});
